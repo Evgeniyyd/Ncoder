ce427d11170008d2e69bed13a9da4ea7dd83b2c8
Evgeniyyd <brai.kel@icloud.com>
2025-04-06 12:34:02 +0300

    refactoring code

--- src/main/java/BruteForce.java
+++ src/main/java/BruteForce.java
@@ -1,15 +1,12 @@
-import javax.imageio.IIOException;
 import java.io.*;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class BruteForce {
     public void bruteForce() {
-        Scanner scanner = new Scanner(System.in);
-        String src = scanner.nextLine();
-        System.out.println("Видите адрес файла куда записать результат");
-        String dst = scanner.nextLine();
+        String src = ConsoleHelper.readString();
+        ConsoleHelper.writeMessage("Видите адрес файла куда записать результат");
+        String dst = ConsoleHelper.readString();
         CaesarCipher cipher = new CaesarCipher();
         try (FileReader fileReader = new FileReader(src);
              BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -22,17 +19,16 @@ public class BruteForce {
             }
             for (int i = 0; i < cipher.getAlphabetLength(); i++) {
                 String encrypt = cipher.decrypt(String.join("", list), i);
-                if (isValidate(encrypt)){
+                if (isValidate(encrypt)) {
                     bufferedWriter.write(encrypt);
                 }
             }
         } catch (IOException i) {
         }
     }
-    private boolean isValidate(String text){
+
+    private boolean isValidate(String text) {
         return false;
     }
 }
 
-//класс java.io.files прочитать заного
-// считать содержимое файла и положить каждую строчку в arrayList
