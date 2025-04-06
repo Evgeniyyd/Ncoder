fb9de95f439190995c935a68d054c48bd586aa32
Evgeniyyd <brai.kel@icloud.com>
2025-04-06 18:57:23 +0300

    добавили реализацию isValidate

--- src/main/java/BruteForce.java
+++ src/main/java/BruteForce.java
@@ -3,7 +3,10 @@ import java.util.ArrayList;
 
 
 public class BruteForce {
+    private static final int MAX_LENGTH_WORD = 28;
+
     public void bruteForce() {
+
         String src = ConsoleHelper.readString();
         ConsoleHelper.writeMessage("Видите адрес файла куда записать результат");
         String dst = ConsoleHelper.readString();
@@ -28,6 +31,28 @@ public class BruteForce {
     }
 
     private boolean isValidate(String text) {
+        String[] words = text.split(" ");
+        for (String word : words) {
+            if (word.length() > MAX_LENGTH_WORD) {
+                return false;
+            }
+        }
+        boolean isValidate = false;
+        if (text.contains(". ") || text.contains(", ") || text.contains("! ") || text.contains("? ")){
+            isValidate = true;
+        }
+        while (isValidate) {
+            ConsoleHelper.writeMessage("понятен ли вам это текст Yes  / No");
+            String answer = ConsoleHelper.readString();
+            if (answer.equalsIgnoreCase("Yes")) {
+                return true;
+            } else if (answer.equalsIgnoreCase("No")) {
+                isValidate = false;
+            } else {
+                ConsoleHelper.writeMessage("Введите Yes или No");
+
+            }
+        }
         return false;
     }
 }
