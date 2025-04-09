9d183d2fd1d216114b180e81a00084e9bcf13f87
Evgeniyyd <brai.kel@icloud.com>
2025-04-09 14:58:24 +0300

    добавление тернарного оператора и использование метода ConsoleHelper

--- src/main/java/EncryptedDecrypted.java
+++ src/main/java/EncryptedDecrypted.java
@@ -1,16 +1,15 @@
-import javax.imageio.IIOException;
 import java.io.*;
-import java.util.Scanner;
+
 
 public class EncryptedDecrypted {
     public void encryptedDecrypted(boolean flag) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите полный адрес для " + (flag ? "раз" : "за") + "шифровки файла");
-        String src = scanner.nextLine();
-        System.out.println("Введите ключь");
-        int key = Integer.parseInt(scanner.nextLine());
-        System.out.println("Введите полный адрес куда записать результат");
-        String dst = scanner.nextLine();
+
+        ConsoleHelper.writeMessage("Введите полный адрес для " + (flag ? "за" : "раз") + "шифровки файла");
+        String src = ConsoleHelper.readString();
+        ConsoleHelper.writeMessage("Введите ключь");
+        int key = ConsoleHelper.readInt();
+        ConsoleHelper.writeMessage("Введите полный адрес куда записать результат");
+        String dst = ConsoleHelper.readString();
         CaesarCipher caesarCipher = new CaesarCipher();
         try (FileReader fileReader = new FileReader(src);
              BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -20,6 +19,7 @@ public class EncryptedDecrypted {
                 String string = bufferedReader.readLine();
                 String result = flag ? caesarCipher.encrypt(string, key) : caesarCipher.decrypt(string, key);
                 bufferedWriter.write(result);
+                bufferedWriter.newLine();
             }
         } catch (IOException ignore) {
         }
